cad27e27e2b133e0ae922bf41b6c40cd259470ae
melisa-abuin <melisa.abuin@gmail.com>
2023-03-18 00:00:16 +0100

    feature #1: add initial next configuration

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Rick and Morty Typescript</title>
+        <meta name="description" content="Test next app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={inter.className}>main content</main>
+    </>
+  );
+}
