9a9a113efeed7d60bc63eb1dace349f0e168455e
melisa-abuin <melisa.abuin@gmail.com>
2023-03-18 14:24:34 +0100

    feature #17: add new layout for homepage

---
+++ src/components/layout/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const CharactersSection = styled.div`
+  align-items: center;
+  background-color: #f2f2f2;
+  display: flex;
+  flex-direction: column;
+  max-width: 100%;
+  min-height: 100vh;
+`
