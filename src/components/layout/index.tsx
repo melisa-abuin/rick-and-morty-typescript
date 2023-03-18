301837322cee89bf1a4137237364bb6eb7b3dc78
melisa-abuin <melisa.abuin@gmail.com>
2023-03-18 14:26:59 +0100

    refactor #17: add spaces among jsx elements in layout component

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -14,8 +14,10 @@ export const Layout = ({ children }: PropsWithChildren) => (
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
+
     <main className={inter.className}>
       <Banner />
+
       <CharactersSection>{children}</CharactersSection>
     </main>
   </>
