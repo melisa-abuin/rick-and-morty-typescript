72e8a4fde4a4060ba7f271ffc46d64d95c114eba
melisa-abuin <melisa.abuin@gmail.com>
2023-03-19 23:00:49 +0100

    test #10: test utility function

---
+++ src/components/paginator/utils/slicePagesArray/__tests__/index.ts
@@ -0,0 +1,30 @@
+import { slicePagesArray } from '..'
+
+const testPages = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+describe('slicePagesArray', () => {
+  it('returns a 5 length array if the current page is the first one', () => {
+    const pages = slicePagesArray(testPages, 0)
+
+    expect(pages).toStrictEqual([0, 1, 2, 3, 4])
+  })
+
+  it('returns a 5 length array if the current page is the last one', () => {
+    const pages = slicePagesArray(testPages, 9)
+
+    expect(pages).toStrictEqual([5, 6, 7, 8, 9])
+  })
+
+  it('returns a 5 length array if the current page is in the middle of the array', () => {
+    const pages = slicePagesArray(testPages, 4)
+
+    expect(pages).toStrictEqual([2, 3, 4, 5, 6])
+  })
+
+  it('returns the default array of pages if the amount of pages is lower than 5', () => {
+    const shortTestPages = [0, 1, 2]
+    const pages = slicePagesArray(shortTestPages, 1)
+
+    expect(pages).toStrictEqual([0, 1, 2])
+  })
+})
